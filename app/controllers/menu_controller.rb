f8a823b243048bc35264d05c1ea87cb2e813dcfb
ShubhankSinghal <shubhank.singhal98@gmail.com>
2021-07-02 02:25:06 +0530

    Improved UI of signin and login

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,6 +1,7 @@
 class MenuController < ApplicationController
   def index
-    #@todos = current_user.todos
     render "index"
   end
+
+
 end
---
+++ app/controllers/owner_controller.rb
@@ -0,0 +1,45 @@
+class OwnerController < ApplicationController
+  def index
+    @todos = current_user.todos
+    render "index"
+  end
+
+  def show
+    id = params[:id]
+    todo = Todo.of_user(current_user).find(id)
+    render "todo"
+  end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = params[:due_date]
+    new_todo = Todo.new(
+      todo_text: todo_text,
+      due_date: due_date,
+      completed: false,
+      user_id: current_user.id,
+    )
+    if new_todo.save
+      redirect_to todos_path
+    else
+      flash[:error] = new_todo.errors.full_messages.join(", ")
+      redirect_to todos_path
+    end
+  end
+
+  def update
+    id = params[:id]
+    completed = params[:completed]
+    todo = Todo.of_user(current_user).find(id)
+    todo.completed = completed
+    todo.save!
+    redirect_to todos_path
+  end
+
+  def destroy
+    id = params[:id]
+    todo = Todo.of_user(current_user).find(id)
+    todo.destroy
+    redirect_to todos_path
+  end
+end
